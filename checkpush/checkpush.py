23bf2a7f7cab379664e2d857621bca8aa6b078ed
Kouni <kouni@LCSE.org>
2015-03-18 02:32:01 +0800

    All in one but sometimes PUBLISH() will hang

--- checkpush/checkpush.py
+++ checkpush/checkpush.py
@@ -6,46 +6,58 @@ NginX Push Stream Module: https://github.com/wandenberg/nginx-push-stream-module
 SUB: curl -s -v 'http://10.78.78.88/sub/978'
 PUB: curl -s -v -X POST 'http://10.78.78.88/pub?id=978' -d 'Hello_Kitty'
 '''
-
-import http.client, urllib.request, urllib.parse, threading, requests
+import http.client, urllib.request, urllib.parse, threading
 
 host = "10.78.78.88"
 channel = "978"
-content = {channel:"OK"}
+content = {host:channel}
+getresult = ""
+threadflag = "0"
 
 class SUBSCRIBE(threading.Thread):
-	def __init__(self, threadID):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-	def run(self):
-		subconn = http.client.HTTPConnection(host)
-		subconn.request('GET', '/sub/'+channel)
-		subresp = subconn.getresponse()
-		subdata = subresp.read()
-		return(subdata)
+    def __init__(self, threadID):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+    def run(self):
+        global getresult
+        global threadflag
+        subconn = http.client.HTTPConnection(host)
+        subconn.request('GET', '/sub/'+channel)
+        subresp = subconn.getresponse()
+        #subdata = subresp.read()
+        #getresult = subdata.decode('UTF-8')
+        if subresp.reason == "OK":
+            threadflag = "1"
+            subdata = subresp.read()
+            getresult = subdata.decode('UTF-8')
+        else:
+            return("Connection Failure")
+            #return(subdata)
 
 def PUBLISH():
-	pubconn = http.client.HTTPConnection(host)
-	pubparams = urllib.parse.urlencode(content)
-	pubconn.request('POST', '/pub?id='+channel, pubparams)
-	pubresp = pubconn.getresponse()
-	#print(pubresp.status, pubresp.reason)
-	pubdata = pubresp.read()
-	return(pubdata.strip())
-	pubconn.close()
+    pubconn = http.client.HTTPConnection(host)
+    pubparams = urllib.parse.urlencode(content)
+    pubconn.request('POST', '/pub?id='+channel, pubparams)
+    pubresp = pubconn.getresponse()
+    pubdata = pubresp.read()
+    print(pubresp.status, pubresp.reason)
+    #return(pubdata.strip())
+    pubconn.close()
 
-threadLock = threading.Lock()
-threads = []
 SubThread = SUBSCRIBE(1)
 SubThread.start()
-
-#count = 0
-
-while(SubThread.isAlive()):
-	PUBLISH()
-#	if subdata == pubdata.strip():
-
-#	count+=1
-#	print(str(count))
-
-SubThread.join()
+print('啟動 Thread')
+#print("FLAG",threadflag)
+while (True):
+    PUBLISH()
+    SubThread.join()
+    print("FLAG",threadflag)
+    #if threadflag == "1":
+    print('送出 Post')
+    if getresult == host+"="+channel:
+        print('SUCCESS')
+    else:
+        print("ERROR")
+        break
+    break
+print("DONE")
